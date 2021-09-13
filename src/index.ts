bff8a84a0f8b2a88948594a32f365073cbb50661
Brian <wl00387719@gmail.com>
2021-09-13 11:17:28 +0800

    feature: config apollo-server and express
    1. Add cors middleware and config origin as localhost:3000 to solve frontend CORS issue
    2. Add "ApolloServerPluginLandingPageGraphQLPlayground" to use graphql playground instead of apollo-server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,46 @@
-console.log('hello')
+import express from 'express'
+import { ApolloServer, gql } from 'apollo-server-express'
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core'
+import cors from 'cors'
+
+async function main () {
+  try {
+    const app = express()
+    app.use(
+      cors({
+        origin: ['http://localhost:3000'],
+        credentials: true
+      })
+    )
+
+    const apolloServer = new ApolloServer({
+      typeDefs: gql`
+        type Query {
+          hello: String
+        }
+      `,
+      resolvers: {
+        Query: {
+          hello: () => {
+            return 'hello world'
+          }
+        }
+      },
+      plugins: [
+        ApolloServerPluginLandingPageGraphQLPlayground()
+      ]
+    })
+    await apolloServer.start()
+    apolloServer.applyMiddleware({
+      app,
+      cors: false
+    })
+    app.listen(4000, () => {
+      console.log('server started on localhost:4000')
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+main()
