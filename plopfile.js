9c0ed8c4a0aecce99ea9b5b99460882ae1fecd1c
Brian <wl00387719@gmail.com>
2021-09-14 14:13:42 +0800

    feature: add model template and fix resolver template

--- plopfile.js
+++ plopfile.js
@@ -1,13 +1,17 @@
+const { readdirSync } = require('fs')
+const path = require('path')
+
+function getModuleList () {
+  const modulePath = path.join(process.cwd(), 'src')
+  return readdirSync(modulePath, { withFileTypes: true })
+    .filter(dirent => dirent.isDirectory() && dirent.name !== 'core')
+    .map(dirent => dirent.name)
+}
+
 const config = (
   /** @type {import('plop').NodePlopAPI} */
   plop
 ) => {
-  plop.setHelper('toUpper', (value) => {
-    if (typeof value === 'string') {
-      return value.toUpperCase()
-    }
-    return value
-  })
   plop.setGenerator('module', {
     description: 'Generate a new module contain resolver',
     prompts: [
@@ -26,7 +30,7 @@ const config = (
       {
         type: 'add',
         path: 'src/{{name}}/index.ts',
-        templateFile: 'templates/module/index.hbs'
+        templateFile: 'templates/module/export-resolver.hbs'
       },
       {
         type: 'append',
@@ -38,8 +42,38 @@ const config = (
       {
         type: 'append',
         path: 'src/core/index.ts',
-        pattern: /export const resolvers = \[/,
+        pattern: /export const resolvers: NonEmptyArray<Function> = \[/,
         templateFile: 'templates/core/export-resolver.hbs'
+      }
+    ]
+  })
+
+  plop.setGenerator('model', {
+    description: 'Generate a new model inside module',
+    prompts: [
+      {
+        type: 'list',
+        name: 'moduleName',
+        message: 'Which module do you want to add this model',
+        choices: getModuleList()
+      },
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Enter the name of the model'
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: 'src/{{moduleName}}/{{name}}-model.ts',
+        templateFile: 'templates/module/model.hbs'
+      },
+      {
+        type: 'append',
+        path: 'src/{{moduleName}}/index.ts',
+        templateFile: 'templates/module/export-model.hbs',
+        separator: ''
       }
     ]
   })
