4246f8fbd303d386cf33df42b208863acdc161cc
Brian <wl00387719@gmail.com>
2021-09-22 15:31:25 +0800

    feature: Adjust auth user middleware and add UserPayload type to MyContext interface

--- src/core/middlewares/auth-user.middleware.ts
+++ src/core/middlewares/auth-user.middleware.ts
@@ -1,30 +1,27 @@
 import { MiddlewareFn } from 'type-graphql'
 import jwt from 'jsonwebtoken'
-import { MyContext } from '../../types';
+import { MyContext, UserPayload } from '../../types';
+
 
 const AUTH_ERR_BY_TYPE = {
   INVALID_TOKEN: { message: 'Invalid/Expired token' },
-  MISS_BEARER_TOKEN: { message: 'Authentication token must be \'Bearer [token]'},
-  MISS_AUTH_HEADER: { message: 'Authentication header must be provided'} 
+  MISS_BEARER_TOKEN: { message: 'Authentication token must be provided. Add token to authorization in header'},
 }
 export const authUser: MiddlewareFn<MyContext> = async ({ context }, next) => {
   const { req } = context
-  const authHeader = req.headers.authorization
-  
-  if (authHeader) {
-    const token = authHeader.split("Bear ")[1];
-    if (token) {
-      try {
-        const user = jwt.verify(token, process.env.SECRET_KEY)
-        context.user = user
-      } catch(e) {
-        context.error = AUTH_ERR_BY_TYPE.INVALID_TOKEN
-      }
-    }
-    context.error = AUTH_ERR_BY_TYPE.MISS_BEARER_TOKEN
-  } else {
-    context.error = AUTH_ERR_BY_TYPE.MISS_AUTH_HEADER
-  }
 
+  const authHeader = req.headers.authorization
+  const token = authHeader ? authHeader.split("Bearer ")[1] : ''
+  if (token) {
+    try {
+      const user = jwt.verify(token, process.env.SECRET_KEY) as UserPayload
+      context.user = user
+    } catch(e) {
+      context.error = AUTH_ERR_BY_TYPE.INVALID_TOKEN
+    }
+  } else {
+    context.error = AUTH_ERR_BY_TYPE.MISS_BEARER_TOKEN
+  }
+  
   return await next();
 };
--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,16 @@
 import { Request } from "express";
 import { ArgumentErrors, AuthError } from './core'
 
+export interface UserPayload {
+  id: string,
+  email: string,
+  username: string,
+  iat: number,
+  exp: number  
+}
 
 export interface MyContext {
   error: ArgumentErrors | AuthError;
   req: Request;
-  user?: any
+  user?: UserPayload
 }
